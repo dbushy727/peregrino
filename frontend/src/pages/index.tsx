e70158433be456516a41af91bcb1ec50ed99d594
Danny Bushkanets <d.bushkanets@gmail.com>
2023-02-27 17:46:37 -0500

    add some blank pages (#1)
    * add some blank pages
    
    * update title
    
    * register -> signup
    
    * add signup link to login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>Laravel</title>
+                <title>Peregrino</title>
             </Head>
 
             <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
@@ -30,7 +30,7 @@ export default function Home() {
                             <Link
                                 href="/auth/register"
                                 className="ml-4 text-sm text-gray-700 underline">
-                                Register
+                                Sign Up
                             </Link>
                         </>
                     )}
